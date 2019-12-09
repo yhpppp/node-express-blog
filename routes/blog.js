41bd44e445ebcfd944b013ec2f8134b6b75cb10b
yhpp <yhpp1995@gmail.com>
2019-12-09 15:07:39 +0800

    feat: 增加response基础类: SuccessModel && ErrorModel

--- routes/blog.js
+++ routes/blog.js
@@ -1,13 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
+const { SuccessModel } = require('../model/resModel')
+
 router.get('/list', (req, res, next) => {
-  res.json({
-    errno: 0,
-    data: {
-      msg: 'hello express'
-    }
-  })
+  res.json(new SuccessModel({a:1,b:2},'list data'))
 })
 
 router.get('/detail', (req, res, next) => {
